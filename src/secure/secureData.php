a078e9a5c3f2b610737df5539967bb8f6f33e9f1
camille <lc.laignel@gmail.com>
2022-11-03 11:26:34 +0100

    database setup

--- src/secure/secureData.php
+++ src/secure/secureData.php
@@ -1,7 +1,7 @@
 <?php
-define("SECURE_DNS_DB", "mysql:localhost=;dbname=luxury");
-define("SECURE_USER_DB", "root");
-define("SECURE_PWD_DB", "klouns8666");
+define("SECURE_DNS_DB", "mysql:host=54.37.31.19;dbname=u223495013_luxury");
+define("SECURE_USER_DB", "u223495013_camille");
+define("SECURE_PWD_DB", "Klouns;8666");
 define('SECURE_EMAIL', "lc.laignel@camei8ht.fr");
 define("SECURE_PWD_EMAIL", "Klouns;8666");
 
